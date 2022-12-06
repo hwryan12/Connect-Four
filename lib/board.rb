d3c23d6a6a568b0d489073c98c251bdd7d920ad4
Harrison Ryan <hwryan12@gmail.com>
2022-12-06 08:17:58 -0600

    add: Add board_gird method and test

--- lib/board.rb
+++ lib/board.rb
@@ -1,37 +1,11 @@
 
-# class Board
-#   def initialize
-#     @letters = ["A", "B", "C", "D", "E", "F", "G"]
-#     @numbers = [1..6]
-#   end
-
-#   def set_board
-#     @board_grid = {}
-#     @letters.each do |let|
-#       @numbers.each do|num|
-#         require "pry"; binding.pry
-#         @board_grid["#{let}#{num}"].to_s = "."
-#       end
-#     end
-#   end
-# end
-
-
 class Board
   def initialize
     @letters = ["A", "B", "C", "D", "E", "F", "G"]
-    @numbers = [1..6]
+    @numbers = [1, 2, 3, 4, 5, 6]
   end
 
-  def set_board
-    @board_grid = {}
-    @letters.each do |let|
-      @numbers.each do|num|
-       @board_grid << let
-       @board_grid << num
-require "pry"; binding.pry
-       @board_grid.flatten!
-      end
-    end
+  def board_grid
+    @letters.product(@numbers)
   end
 end
