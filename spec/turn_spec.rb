968c5677f8e3b4ac0d1562a701546d6032fea669
Harrison Ryan <hwryan12@gmail.com>
2022-12-09 20:20:28 -0600

    refactor: Refactor test for turnspec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -12,46 +12,98 @@ RSpec.describe Turn do
     expect(turn).to be_an_instance_of(Turn)
     end
 
-    it "each player starts out with 21 pieces" do 
+
+    it "each player starts out with 21 pieces" do
       turn = Turn.new(@board)
       humanpieces = Piece.new(:human)
       computerpieces = Piece.new(:computer)
-      player= Player.new(:human, humanpieces)
-      computer= Player.new(:computer, computerpieces)
-
+      player = Player.new(:human, humanpieces)
+      computer = Player.new(:computer, computerpieces)
+      
       expect(player.pieces).to eq(21)
       expect(computer.pieces).to eq(21)
     end
 
-    it "after each turn the participants' tokens diminish by one" do 
+    it "each player can place a token" do
       turn = Turn.new(@board)
       humanpieces = Piece.new(:human)
       computerpieces = Piece.new(:computer)
-      player= Player.new(:human, humanpieces)
-      computer= Player.new(:computer, computerpieces)
+      player = Player.new(:human, humanpieces)
+      computer = Player.new(:computer, computerpieces)
+
       turn.place_token(:human)
+      turn.place_token(:computer)
+      
+      expect(board.board_grid).not_to eq()
+      # Come back here to add board rendered
+    end
+    
+    it "after each turn the participants' tokens diminsh by one" do
+      turn = Turn.new(@board)
+      humanpieces = Piece.new(:human)
+      computerpieces = Piece.new(:computer)
+      player = Player.new(:human, humanpieces)
+      computer = Player.new(:computer, computerpieces)
+
+      turn.choose_column(:human)
+      turn.place_token(:human)
+      turn.choose_column(:computer)
       turn.place_token(:computer)
 
       expect(player.pieces).to eq(20)
       expect(computer.pieces).to eq(20)
+    end
 
-    end 
-    # it "each player can place a token" do 
+    # it "can display the board with placed token" do
     #   turn = Turn.new(@board)
-    #   require 'pry'; binding.pry
-    #   turn.choose_column(:human).to eq(@choice)
+    #   humanpieces = Piece.new(:human)
+    #   computerpieces = Piece.new(:computer)
+    #   player = Player.new(:human, humanpieces)
+    #   computer = Player.new(:computer, computerpieces)
 
+    #   turn.choose_column(:human)
+    #   turn.place_token(:human)
+    #   turn.place_token(:computer)
 
-
-    # end 
-
-
-    #   #  Human chooses token- no test here. 
-    # it "placing the token will change the board" do
-    #   turn = Turn.new(@board)
-    #   turn.computer_choice
-    #   expect(turn.computer_choice).to eq("A" || "B" || "C" || "D" || "E" || "F"|| "G")
+    #   expect(turn.@board.board_grid).to
+    #   # What are we expecting here
     # end
+
+        # Needs work
+    it "can check if the player won vertically" do
+      turn = Turn.new(@board)
+      humanpieces = Piece.new(:human)
+      computerpieces = Piece.new(:computer)
+      player = Player.new(:human, humanpieces)
+      computer = Player.new(:computer, computerpieces)
+
+      turn.play_turn
+      expect(turn.check_winner.vertical).to be false
+    end
+  
+        # Needs work
+    it "can check if the player won horizontally" do
+      turn = Turn.new(@board)
+      humanpieces = Piece.new(:human)
+      computerpieces = Piece.new(:computer)
+      player = Player.new(:human, humanpieces)
+      computer = Player.new(:computer, computerpieces)
+
+      turn.play_turn
+      expect(turn.check_winner.horizontal).to be false
+    end
+  
+    # Needs work
+    it "can check if the player won diagonally" do
+      turn = Turn.new(@board)
+      humanpieces = Piece.new(:human)
+      computerpieces = Piece.new(:computer)
+      player = Player.new(:human, humanpieces)
+      computer = Player.new(:computer, computerpieces)
+
+      turn.play_turn
+      expect(turn.check_winner.diagonally).to be false
+    end
 
     it "can check if the game draws" do
       turn = Turn.new(@board)
