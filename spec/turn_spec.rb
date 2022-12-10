b0d8290b3747a00e88782c053ac80abaf091f5a6
Hady Matar <hadyematar@gmail.com>
2022-12-09 17:39:56 -0700

    Hady's started changes on testing and turn.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -12,58 +12,47 @@ RSpec.describe Turn do
     expect(turn).to be_an_instance_of(Turn)
     end
 
-
-    it "the computer will choose one of several random choices" do
+    it "each player starts out with 21 pieces" do 
       turn = Turn.new(@board)
-      turn.computer_choice
-      expect(turn.computer_choice).to eq("A" || "B" || "C" || "D" || "E" || "F"|| "G")
+      humanpieces = Piece.new(:human)
+      computerpieces = Piece.new(:computer)
+      player= Player.new(:human, humanpieces)
+      computer= Player.new(:computer, computerpieces)
+
+      expect(player.pieces).to eq(21)
+      expect(computer.pieces).to eq(21)
     end
 
-  # # Does not pass
-  #   xit "can have the human player place the token" do
-    
-  #     turn = Turn.new(@board)
-  #     turn.human_choice.choice == "A"
+    it "after each turn the participants' tokens diminish by one" do 
+      turn = Turn.new(@board)
+      humanpieces = Piece.new(:human)
+      computerpieces = Piece.new(:computer)
+      player= Player.new(:human, humanpieces)
+      computer= Player.new(:computer, computerpieces)
+      turn.choose_column(:human)
+      turn.place_token(:human)
+      turn.place_token(:computer)
 
-  #     expect(turn.a_count). to eq(1)
-  #   end 
-  
-  #   # Does not pass
-  #   xit "can determine winner for diagonal" do
-  #     turn = Turn.new(@board)
-  #     turn.diagonal(diagonal_pieces)
+      expect(player.pieces).to eq(20)
+      expect(computer.pieces).to eq(21)
 
-  #     expect(turn.check_winner)to eq(diagonal)
-  #   end
+    end 
+    # it "each player can place a token" do 
+    #   turn = Turn.new(@board)
+    #   require 'pry'; binding.pry
+    #   turn.choose_column(:human).to eq(@choice)
 
-  #  # Does not pass
-  #   xit "can determine winner for vertical" do
-  #     turn = Turn.new(@board)
-  #     turn.vertical(column)
-    
-  #     expect(turn.check_winner)to eq(vertical)
-  #   end
 
-  #  # Does not pass
-  #   xit "can determine winner for horizontal" do
-  #     turn = Turn.new(@board)
-  #     turn.horizontal(row)
-      
-  #     expect(turn.check_winner)to eq(horizontal)
-  #   end
-  
-  #  # Does not pass
-  #   xit "can determine a draw" do
-  #     turn = Turn.new(@board)
-  #     turn.draw(a, b, c, d, e, f)
-    
-  #     expect(turn.check_winner)to eq(draw)
-  #   end
-  
-  #   xit "can determine if four pieces are in a row" do
-  #     turn = Turn.new(@board)
 
-  #     turn.four_in_a_row(pieces)
-  #   end
+    # end 
+
+
+    #   #  Human chooses token- no test here. 
+    # it "placing the token will change the board" do
+    #   turn = Turn.new(@board)
+    #   turn.computer_choice
+    #   expect(turn.computer_choice).to eq("A" || "B" || "C" || "D" || "E" || "F"|| "G")
+    # end
+
 
 end
