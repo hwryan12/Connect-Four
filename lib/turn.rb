1efaf5adc65c1a5f1adeb5ed29ede89d6b7771a9
Hady Matar <hadyematar@gmail.com>
2022-12-10 21:40:32 -0700

    Updated tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,13 @@
 class Turn 
-    attr_reader :turn, :a_count, :b_count, :c_count, :d_count, :e_count, :f_count, :g_count, :board 
+    attr_reader :turn, :a_count, :b_count, :c_count, :d_count, :e_count, :f_count, :g_count, :board, :computer, :player 
     attr_accessor :choice
 
     def initialize(board)
         @board = board
-        humanpieces = Piece.new(:human)
-        computerpieces = Piece.new(:computer)
-        player= Player.new(:human, humanpieces)
-        computer= Player.new(:computer, computerpieces)
-        @player = player
-        @computer = computer
+        @humanpieces = Piece.new(:human)
+        @computerpieces = Piece.new(:computer)
+        @player= Player.new(:human, @humanpieces)
+        @computer= Player.new(:computer, @computerpieces)
         @choice = nil
         @a_count = 0 
         @b_count = 0
@@ -18,6 +16,7 @@ class Turn
         @e_count = 0
         @f_count = 0
         @g_count = 0
+        @victor == nil 
     end 
 
     def play_turn 
@@ -48,9 +47,9 @@ class Turn
                 E
                 F
                 G"
-            @choice = gets.chomp
+                @choice = gets.chomp
         elsif type == :computer
-            @choice = ["A", "B", "C", "D", "E", "F", "G"].sample
+                @choice = ["A", "B", "C", "D", "E", "F", "G"].sample
         end
     end 
 
@@ -58,11 +57,9 @@ class Turn
 
         if type == :human
             if @choice == "A"
-                # require 'pry'; binding.pry
                 if @a_count >= 6 
                     puts "This row is filled up. Try another!"
                     choose_column(:human)
-                    # require 'pry'; binding.pry
                 else @board.board[:A].reverse.find do |letter|
                     if letter == (".")
                         letter.replace("X")
@@ -223,88 +220,82 @@ class Turn
     end 
     
     def check_winner
+        require 'pry'; binding.pry
+        vertical_win
+        horizontal_win
+        diagonal_win
+        draw
 
-        self.vertical_win?
-        self.horizontal_win?
-        self.diagonal_win?
-        self.draw
-        # require 'pry'; binding.pry
+        if @victor != nil 
+            print_winner(@victor)
+            self.new_game
+        end
+    end 
 
-
-        if @winner == @computer 
-            puts "The Computer Connected 4-- don't worry, you could one day design a program where you always win!"
-        elsif @winner == @player
-            puts "You connected 4 and won-- are you sure you didn't make a program where you always win? Either way, pat yourself on the back."
-        elsif @winner == @draw
-            puts "This game has ended in a draw. Instead of feeling sad, feel happy! Congrats! You are the equal match of two aspiring programmers at Turing. So that has to mean something."
-        else puts "Next Turn"
-        end 
-       
-    end
-    
-    def four_in_a_row(array)
-       new_array = []
-    #    require 'pry'; binding.pry
-        array.each do |element|
-            new_array << element.join("")
-            if new_array.include?("XXXX")
-                true 
-            else false 
-            end 
-            if new_array.include?("OOOO")
-                true
-            else false
-            end
+    def draw  
+        if @player.pieces == 0 && @computer.pieces == 0
+            puts "Draw! There's no more pieces."
+            puts "You can try again!"
+            @victor == "draw"
         end
     end
-    
-    def vertical_win?
+
+    def print_winner(victor)
         # require 'pry'; binding.pry
+        if victor == @computer
+            puts "The Computer Connected 4-- don't worry, you could one day design a program where you always win!" 
+            "The Computer Connected 4-- don't worry, you could one day design a program where you always win!"
+        elsif victor == @player
+            puts "You connected 4 and won-- are you sure you didn't make a program where you always win? Either way, pat yourself on the back." 
+            "You connected 4 and won-- are you sure you didn't make a program where you always win? Either way, pat yourself on the back."
+        elsif victor == "draw"
+            puts "This game has ended in a draw. Instead of feeling sad, feel happy! Congrats! You are the equal match of two aspiring programmers at Turing. So that has to mean something." 
+            "This game has ended in a draw. Instead of feeling sad, feel happy! Congrats! You are the equal match of two aspiring programmers at Turing. So that has to mean something."
+        end 
+    end
+    
+    def vertical_win
         @all_arrays = [@board.a, @board.b, @board.c, @board.d, @board.e, @board.f, @board.g]
 
         @all_arrays.each do |array|
             vert_array = array.join("")
             if vert_array.include?("XXXX")
-                # puts "Connected 4 Vertically!"
-                # puts "Player Wins!"
-                # new_game = Game.new
-                # new_game.start_game
+                puts "Connected 4 Vertically!"
+                puts "Player Wins!"
+                @victor = "human"
             elsif vert_array.include?("OOOO")
-                # puts "Connected 4 Vertically!"
-                # puts "The Computer Wins!"
-                # new_game = Game.new
-                # new_game.start_game
-            end
+                puts "Connected 4 Vertically!"
+                puts "The Computer Wins!"
+                @victor = "computer"
             end
         end
     end
-    def horizontal_win?
 
-        all_arrays2 = []
-        all_arrays2 << [@board.a[5], @board.b[5], @board.c[5], @board.d[5], @board.e[5], @board.f[5], @board.g[5]] 
-        all_arrays2 << [@board.a[4], @board.b[4], @board.c[4], @board.d[4], @board.e[4], @board.f[4], @board.g[4]] 
-        all_arrays2 << [@board.a[3], @board.b[3], @board.c[3], @board.d[3], @board.e[3], @board.f[3], @board.g[3]] 
-        all_arrays2 << [@board.a[2], @board.b[2], @board.c[2], @board.d[2], @board.e[2], @board.f[2], @board.g[2]] 
-        all_arrays2 << [@board.a[1], @board.b[1], @board.c[1], @board.d[1], @board.e[1], @board.f[1], @board.g[1]] 
-        all_arrays2 << [@board.a[0], @board.b[0], @board.c[0], @board.d[0], @board.e[0], @board.f[0], @board.g[0]] 
+    def horizontal_win
+
+        horizontal_arrays = []
+        horizontal_arrays << [@board.a[5], @board.b[5], @board.c[5], @board.d[5], @board.e[5], @board.f[5], @board.g[5]] 
+        horizontal_arrays << [@board.a[4], @board.b[4], @board.c[4], @board.d[4], @board.e[4], @board.f[4], @board.g[4]] 
+        horizontal_arrays << [@board.a[3], @board.b[3], @board.c[3], @board.d[3], @board.e[3], @board.f[3], @board.g[3]] 
+        horizontal_arrays << [@board.a[2], @board.b[2], @board.c[2], @board.d[2], @board.e[2], @board.f[2], @board.g[2]] 
+        horizontal_arrays << [@board.a[1], @board.b[1], @board.c[1], @board.d[1], @board.e[1], @board.f[1], @board.g[1]] 
+        horizontal_arrays << [@board.a[0], @board.b[0], @board.c[0], @board.d[0], @board.e[0], @board.f[0], @board.g[0]] 
         
-        all_arrays2.each do |array|
-            horiz_array = array.join("")
-            if horiz_array.include?("XXXX")
-                # puts "Connected 4 Horizontally!"
-                # puts "Player Wins!"
-                # new_game = Game.new
-                # new_game.start_game
-            elsif horiz_array.include?("OOOO")
-                # puts "Connected 4 Horizontally!"
-                # puts "The Computer Wins!"
-                # new_game = Game.new
-                # new_game.start_game
+        horizontal_arrays.each do |array|
+            horiz_array_to_check = array.join("")
+            if horiz_array_to_check.include?("XXXX")
+                puts "Connected 4 Horizontally!"
+                puts "Player Wins!"
+                @victor == @player
+            elsif horiz_array_to_check.include?("OOOO")
+                puts "Connected 4 Horizontally!"
+                puts "The Computer Wins!"
+                @victor == @computer
             end
         end
     end
 
-    def diagonal_win?
+    def diagonal_win
         diag_array = []
         diag_array << [@board.a[2], @board.b[3], @board.c[4], @board.d[5]]
         diag_array << [@board.a[1], @board.b[2], @board.c[3], @board.d[4]]
@@ -334,25 +325,14 @@ class Turn
         diag_array.each do |array|
             diag_array2 = array.join("")
             if diag_array2.include?("XXXX")
-                # puts "Connected 4 Diagonally!"
-                # puts "Player Wins!"
-                # new_game = Game.new
-                # new_game.start_game
+                puts "Connected 4 Diagonally!"
+                puts "Player Wins!"
+                @victor == @player
             elsif diag_array2.include?("OOOO")
-                # puts "Connected 4 Diagonally!"
-                # puts "The Computer Wins!"
-                # new_game = Game.new
-                # new_game.start_game
+                puts "Connected 4 Diagonally!"
+                puts "The Computer Wins!"
+                @victor == @computer
             end
-        end
-    end
-
-    def draw  
-        if @player.pieces == 0 && @computer.pieces == 0
-            # puts "Draw! There's no more pieces."
-            # puts "You can try again!"
-            # new_game = Game.new
-            # new_game.start_game
         end
     end
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -23,14 +23,18 @@ RSpec.describe Turn do
       expect(computer.pieces).to eq(21)
     end
 
+    it "each turn creates the pieces and the players" do 
+      board = Board.new
+      turn = Turn.new(board)
+
+      expect(turn.instance_variables).to include(:@humanpieces, :@computerpieces, :@player, :@computer, :@a_count, :@b_count, :@c_count, :@d_count, :@e_count, :@f_count, :@g_count, :@board)
+    end
+
 
     it "the board by default is empty before anyone plays on a turn" do
       board = Board.new
       turn = Turn.new(board)
-      humanpieces = Piece.new(:human)
-      computerpieces = Piece.new(:computer)
-      player = Player.new(:human, humanpieces)
-      computer = Player.new(:computer, computerpieces)
+
       empty_board = {"A": [".", ".", ".", ".", ".", "."], 
         "B": [".", ".", ".", ".", ".", "."],  
         "C": [".", ".", ".", ".", ".", "."],
@@ -43,31 +47,32 @@ RSpec.describe Turn do
       
     end
 
-    it "each player can place a token" do
+    it "a token placed in A will increase the count of A by 1" do 
       board = Board.new
       turn = Turn.new(board)
-      
-    # it "each player can place a token" do
-    #   turn = Turn.new(@board)
-    #   humanpieces = Piece.new(:human)
-    #   computerpieces = Piece.new(:computer)
-    #   player = Player.new(:human, humanpieces)
-    #   computer = Player.new(:computer, computerpieces)
 
-    #   turn.place_token(:human)
-    #   turn.place_token(:computer)
-      
-    #   expect(board.board_grid).not_to eq()
-      # Come back here to add board rendered
-    # end
-    
-    xit "after each turn the participants' tokens diminsh by one" do
-      turn = Turn.new(@board)
+      turn.choice = "A"
+      turn.place_token(:computer)
 
-      humanpieces = Piece.new(:human)
-      computerpieces = Piece.new(:computer)
-      player = Player.new(:human, humanpieces)
-      computer = Player.new(:computer, computerpieces)
+      expect(turn.a_count).to eq(1)
+
+    end 
+
+    it "a token placed in D will increase the count of A by 1" do 
+      board = Board.new
+      turn = Turn.new(board)
+
+      turn.choice = "D"
+      turn.place_token(:computer)
+
+      expect(turn.d_count).to eq(1)
+
+    end 
+
+    it "after each turn the board changes" do
+      board = Board.new
+      turn = Turn.new(board)
+
       turn.choose_column(:computer)
       turn.place_token(:computer)
       empty_board = {"A": [".", ".", ".", ".", ".", "."], 
@@ -82,109 +87,89 @@ RSpec.describe Turn do
       
     end
 
-
-    it "a token placed in A will increase the count of A by 1" do 
+    it "a computer token will be an O and be placed at the end of the array " do
+# Note the reverse function on the turn.rb method place_token(type)- this was a creation of the way the board prints. 
       board = Board.new
       turn = Turn.new(board)
-      humanpieces = Piece.new(:human)
-      computerpieces = Piece.new(:computer)
-      player = Player.new(:human, humanpieces)
-      computer = Player.new(:computer, computerpieces)
-      turn.choice = "A"
+
+      turn.choice = "D"
       turn.place_token(:computer)
+      transformed_board = {"A": [".", ".", ".", ".", ".", "."], 
+      "B": [".", ".", ".", ".", ".", "."],  
+      "C": [".", ".", ".", ".", ".", "."],
+      "D": [".", ".", ".", ".", ".", "O"], 
+      "E": [".", ".", ".", ".", ".", "."], 
+      "F": [".", ".", ".", ".", ".", "."],
+      "G": [".", ".", ".", ".", ".", "."]}
+      
+      expect(board.board).to eq(transformed_board)
+      
+    end
 
-      expect(turn.a_count).to eq(1)
+    it "a human token will be an X and be placed at the end of the array " do
+  # Note the reverse function on the turn.rb method place_token(type)- this was a creation of the way the board prints. 
+      board = Board.new
+      turn = Turn.new(board)
+     
+      turn.choice = "A"
+      turn.place_token(:human)
+      transformed_board = {"A": [".", ".", ".", ".", ".", "X"], 
+      "B": [".", ".", ".", ".", ".", "."],  
+      "C": [".", ".", ".", ".", ".", "."],
+      "D": [".", ".", ".", ".", ".", "."], 
+      "E": [".", ".", ".", ".", ".", "."], 
+      "F": [".", ".", ".", ".", ".", "."],
+      "G": [".", ".", ".", ".", ".", "."]}
+            
+       expect(board.board).to eq(transformed_board)
+            
+    end
+    # EVERYTHING BELOW HERE WE STILL NEED TO GET TO RUN 
 
-    end 
-    
-    # it "after each turn the participants' tokens diminsh by one" do
-    #   turn = Turn.new(@board)
-    #   humanpieces = Piece.new(:human)
-    #   computerpieces = Piece.new(:computer)
-    #   player = Player.new(:human, humanpieces)
-    #   computer = Player.new(:computer, computerpieces)
+    it "it will print a message for the computer winner" do 
+      board = Board.new
+      turn = Turn.new(board)
 
-    #   turn.choose_column(:human)
-    #   turn.place_token(:human)
-    #   turn.choose_column(:computer)
-    #   turn.place_token(:computer)
+      expect(turn.print_winner(turn.computer)).to eq("The Computer Connected 4-- don't worry, you could one day design a program where you always win!")
+    end
 
-    #   expect(player.pieces).to eq(20)
-    #   expect(computer.pieces).to eq(20)
-    # end
+    it "it will print a message for the human winner" do 
+      board = Board.new
+      turn = Turn.new(board)
+     
 
-    # it "a game participant will not be able to place a token in a full column" do 
-    #   turn = Turn.new(@board)
-    #   humanpieces = Piece.new(:human)
-    #   computerpieces = Piece.new(:computer)
-    #   player = Player.new(:human, humanpieces)
-    #   computer = Player.new(:computer, computerpieces)
-    #   @a_count = 6
-    #   @choice = "A"
-    #   require 'pry'; binding.pry
+      expect(turn.print_winner(turn.player)).to eq("You connected 4 and won-- are you sure you didn't make a program where you always win? Either way, pat yourself on the back.")
+    end
 
-    #   expect(turn.place_token(:human).choice).to eq("This row is filled up. Try another!")
+    it "it will print a draw" do 
+      board = Board.new
+      turn = Turn.new(board)
 
+      expect(turn.print_winner("draw")).to eq("This game has ended in a draw. Instead of feeling sad, feel happy! Congrats! You are the equal match of two aspiring programmers at Turing. So that has to mean something.")
+    end
 
-    # end 
+    it "if four XXXXs are connected vertically, the player is declared the victor" do 
 
+    end
 
-    #     # Needs work
-    # it "can check if the player won vertically" do
-    #   turn = Turn.new(@board)
-    #   humanpieces = Piece.new(:human)
-    #   computerpieces = Piece.new(:computer)
-    #   player = Player.new(:human, humanpieces)
-    #   computer = Player.new(:computer, computerpieces)
+    it "if four OOOOs are connected vertically, the computer is declared the victor" do 
+      
+    end
 
-    #   turn.play_turn
-    #   expect(turn.vertical_win?).to be false
+    it "if four XXXXs are connected horizontally, the player is declared the victor" do 
 
-    #   four_in_a_row(@vert_arrays) == true
-    #   expect(turn.vertical_win?).to be true
-    # end
-  
-    #   it "can check if the player won horizontally" do
-    #   turn = Turn.new(@board)
-    #   humanpieces = Piece.new(:human)
-    #   computerpieces = Piece.new(:computer)
-    #   player = Player.new(:human, humanpieces)
-    #   computer = Player.new(:computer, computerpieces)
+    end
 
-    #   turn.play_turn
-    #   expect(turn.horizontal_win?).to be false
+    it "if four OOOOs are connected horizontally, the player is declared the victor" do 
 
+    end
 
-    #   four_in_a_row(@horiz_arrays) == true
-    #   expect(turn.horizontal_win?).to be true
-    # end
-  
-    # it "can check if the player won diagonally" do
-    #   turn = Turn.new(@board)
-    #   humanpieces = Piece.new(:human)
-    #   computerpieces = Piece.new(:computer)
-    #   player = Player.new(:human, humanpieces)
-    #   computer = Player.new(:computer, computerpieces)
+    it "if four XXXXs are connected diagonally, the player is declared the victor" do 
 
+    end
 
-    #   turn.play_turn
-    #   expect(turn.diagonal_win?).to be false
+    it "if four OOOOs are connected diagonally, the player is declared the victor" do 
 
-    #   four_in_a_row(@diag_arrays) == true
-    #   expect(turn.diagonal_win?).to be true
-    # end
-
-    # it "can check if the game draws" do
-    #   turn = Turn.new(@board)
-    #   humanpieces = Piece.new(:human)
-    #   computerpieces = Piece.new(:computer)
-    #   player = Player.new(:human, humanpieces)
-    #   computer = Player.new(:computer, computerpieces)
-
-    #   @player.pieces == 0
-    #   @computer.pieces == 0
-    #   expect(turn.check_winner.draw).to be true
-    # end
-
+    end
 
 end
